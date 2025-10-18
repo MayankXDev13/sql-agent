552fb9d28684c151821b5c4d142867ce86110eec
Mayank <vijaymaynk2003@gmail.com>
2025-10-18 17:44:40 +0530

    improve ui

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,12 @@
 import { db } from "@/db/db";
 import { openai } from "@ai-sdk/openai";
-import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from "ai";
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+} from "ai";
 import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
@@ -9,19 +15,31 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
-  const SYSTEM_PROMT = `You are an expert SQl assistan that helps users to quey theri database using natural language.
+  const SYSTEM_PROMT = `You are an expert SQL assistant that helps users query their databases using natural language.
+Current Date & Time: ${new Date().toLocaleString("sv-SE")}
 
-  ${new Date().toLocaleString('sv-SE')}
-  You have access to following tools:
-  1. schema tool - call this tool to get the database schema which will help you write sql query
-  2. db toll - call this tool to quey the database
+🧰 Available Tools
 
-  Rules:
-  - Generate ONLY SELECT queries (no INSERT, UPDATE, DELETE, DROP)
-  - Always use the schema provided by the schema tool
-  - Pass in valid SQL syntax in do tool
+Schema Tool – Use this to retrieve the database schema before writing any queries.
 
-  Always repond is helpfull conversation tone while being technically accurate
+DB Tool – Use this to execute SQL queries against the database.
+
+⚙️ Rules
+
+✅ Only generate SELECT queries.
+(Never use INSERT, UPDATE, DELETE, or DROP.)
+
+🧩 Always reference the schema provided by the Schema Tool before writing a query.
+
+🧠 Ensure all generated queries are valid SQL syntax and compatible with the given database.
+
+💬 Respond in a helpful, conversational, and technically accurate tone.
+
+🪶 Keep explanations clear and concise — make sure the user understands what the query does.
+
+🎯 Goal
+
+Help users transform natural language requests into accurate SQL queries, explaining your reasoning briefly when needed.
   `;
 
   const result = streamText({
@@ -40,14 +58,12 @@ export async function POST(req: Request) {
           // string search [delete, update] -> Guardrails
 
           return await db.run(query);
-
         },
       }),
       schema: tool({
         description: "Call this tool to get database schema information",
         inputSchema: z.object(),
         execute: async () => {
-
           return `
           CREATE TABLE products (
           id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -67,8 +83,7 @@ export async function POST(req: Request) {
           region text NOT NULL,
           FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE no action ON DELETE no action
         );
-          `
-
+          `;
         },
       }),
     },
